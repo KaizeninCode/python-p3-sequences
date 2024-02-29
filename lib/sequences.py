51242cfa889716300f571bee2f59bf21c2cdc9df
Phil <philwekullo@gmail.com>
2024-02-29 14:15:44 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
-
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    if n == 0:
+        print('[]')
+    elif n == 1:
+        print('[0]')
+    elif n == 2:
+        print('[0, 1]')
+    else:
+        a, b, fib_seq = 0, 1, []
+        for i in range(n):
+            fib_seq.append(a)
+            a, b = b, a + b
+        print(fib_seq)
